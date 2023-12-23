ccfe3f9249a5b1f71fce594816d39a3d079769cb
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-12-23 12:04:16 +0000

    .

---
+++ lq/kch.fish
@@ -0,0 +1,10 @@
+#!/bin/fish
+/workspaces/lq-PostGresSqlPanty/lq/pnp.fish
+/workspaces/lq-PostGresSqlPanty/lq/fixrs.fish
+/workspaces/lq-PostGresSqlPanty/lq/st.sh
+/workspaces/lq-PostGresSqlPanty/lq/aryt.fish
+/workspaces/lq-PostGresSqlPanty/lq/tgpt.fish
+/workspaces/lq-PostGresSqlPanty/lq/bun.fish
+/workspaces/lq-PostGresSqlPanty/lq/tmuxx.fish
+
+
