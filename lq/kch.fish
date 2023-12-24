0a636e9cbf4a21472713a0c6f1d83c8cef2cc3cd
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-12-24 14:12:56 +0000

    .

---
+++ lq/kch.fish
@@ -0,0 +1,10 @@
+#!/bin/fish
+/workspaces/sn-cy1/lq/pnp.fish
+/workspaces/sn-cy1/lq/fixrs.fish
+/workspaces/sn-cy1/lq/st.sh
+/workspaces/sn-cy1/lq/aryt.fish
+/workspaces/sn-cy1/lq/tgpt.fish
+/workspaces/sn-cy1/lq/bun.fish
+/workspaces/sn-cy1/lq/tmuxx.fish
+
+
